56f143096e77992fbcb40cbd12c637fc55b4e4d1
Peng <pjiang@superbio.ai>
2022-11-03 21:20:50 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="app-sbioutils",
+      version="1.0.0",
+      description="SBIO app runner utils",
+      license='MIT',
+      author="",
+      authoer_email="",
+      url='https://github.com/Superbio-ai/app-sbioutils',
+      install_requires=[boto3, requests]
+)
