7fe06260af0b12aa6478b21507e19a55a10d6ae4
superbio-ds <85495534+superbio-ds@users.noreply.github.com>
2023-05-03 16:07:02 +0100

    add file process, update requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name="sbioapputils",
-      version="1.0.0",
+      version="1.0.1",
       description="SBIO app runner utils",
       license='MIT',
-      author="",
+      author="superbio.ai",
       authoer_email="",
       url='https://github.com/Superbio-ai/app-sbioutils',
-      install_requires=['requests==2.22.0', 'boto3==1.21.27'],
+      install_requires=['requests==2.22.0', 'boto3==1.21.27','pandas==1.2.5', 'anndata==0.8.0', 'scanpy==1.9.1', 'numpy==1.21'],
       packages=['sbioapputils']
 )
