0c0bdad3cf62fc31d9b4bb9adbaf5098ef613e3e
superbio-ds <85495534+superbio-ds@users.noreply.github.com>
2023-05-26 15:48:05 +0100

    change packages

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name="sbioapputils",
       author="Superbio AI",
       author_email="smorgan@superbio.ai",
       url='https://github.com/Superbio-ai/app-sbioutils',
-      install_requires=['requests>=2.22.0', 'boto3>=1.21.27','yaml'],
+      install_requires=['requests==2.22.0', 'boto3==1.21.27'],
       packages=['sbioapputils']
 )
