83303e7a59061a135d4d24fcc9a008733cceab32
superbio-ds <85495534+superbio-ds@users.noreply.github.com>
2023-05-30 18:19:33 +0100

    Fix float min and max

--- sbioapputils/app_runner/workflow_utils.py
+++ sbioapputils/app_runner/workflow_utils.py
@@ -71,10 +71,10 @@ def validate_request(request, parameters):
             if parameters[key]['type'] in ['int', 'float']:
                 # Check between min and max
                 if parameters[key].get('max_value'):
-                    if request[key] > parameters[key]['max_value']:
+                    if request[key] > float(parameters[key]['max_value']):
                         invalid_value.append(key)
                 if parameters[key].get('min_value'):
-                    if request[key] < parameters[key]['min_value']:
+                    if request[key] < float(parameters[key]['min_value']):
                         invalid_value.append(key)
 
             elif eval(parameters[key]['type']) == 'str':
