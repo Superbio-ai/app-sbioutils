76d64bbf51c54adaf1bb3ec111818fcde2f3a436
superbio-ds <85495534+superbio-ds@users.noreply.github.com>
2023-06-15 11:45:26 +0100

    Change workflow copy

--- sbioapputils/app_runner/workflow_utils.py
+++ sbioapputils/app_runner/workflow_utils.py
@@ -11,7 +11,7 @@ def parse_workflow(request):
         src_file = f"app/{request['workflow_name']}.yml"
         if (os.path.exists(workflow_loc)) and not (os.path.samefile(src_file, workflow_loc)):
             os.remove(workflow_loc)
-        shutil.copy(src_file,workflow_loc)
+            shutil.copy(src_file,workflow_loc)
         
     with open(workflow_loc, "r") as stream:
         try:
