301a82f548acfc531f4f47211febbe7ce0f128e9
Kateryna Dubinina <ururruu@gmail.com>
2023-09-13 18:56:06 +0300

    Fix bugs [1.0.24] (#16)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
       name="sbioapputils",
-      version="1.0.23",
+      version="1.0.24",
       description="Superbio app runner utils",
       author="Superbio AI",
       author_email="smorgan@superbio.ai",
