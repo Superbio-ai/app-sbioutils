d481b152ff41e9027d445c0b8d9cc52e6f129c6f
Kateryna Dubinina <ururruu@gmail.com>
2023-10-12 16:03:13 +0300

    fix intgeration 1.0.31

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
       name="sbioapputils",
-      version="1.0.30",
+      version="1.0.31",
       description="Superbio app runner utils",
       author="Superbio AI",
       author_email="smorgan@superbio.ai",
