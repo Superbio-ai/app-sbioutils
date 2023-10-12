8ce0926c94e96be149b9da4dac5297e3e1638236
Kateryna Dubinina <ururruu@gmail.com>
2023-10-12 13:43:27 +0300

    1.0.30 (#24)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
       name="sbioapputils",
-      version="1.0.29",
+      version="1.0.30",
       description="Superbio app runner utils",
       author="Superbio AI",
       author_email="smorgan@superbio.ai",
